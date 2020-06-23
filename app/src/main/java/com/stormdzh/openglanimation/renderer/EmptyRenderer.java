ce837e9e53dd401f2c89d62b26f0103911a41234
dzh <411788973@qq.com>
2020-06-23 09:44:39 +0800

    测试代码

---
+++ app/src/main/java/com/stormdzh/openglanimation/renderer/EmptyRenderer.java
@@ -0,0 +1,28 @@
+package com.stormdzh.openglanimation.renderer;
+
+import android.opengl.GLSurfaceView;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+/**
+ * @Description: 测试Renderer
+ * @Author: dzh
+ * @CreateDate: 2020-06-22 17:33
+ */
+public class EmptyRenderer implements GLSurfaceView.Renderer {
+    @Override
+    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl, int width, int height) {
+
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl) {
+
+    }
+}
